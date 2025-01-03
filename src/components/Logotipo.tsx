59c2a902166459ffc38fdf981fbe976d8e93d1d9
Brandon Pinzon <brandonpinzont@gmail.com>
2025-01-03 18:55:15 -0500

    Add logotipo

---
+++ src/components/Logotipo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logotipo = () => {
+  return (
+    <div className="bg-[#80762C] p-1 lg:p-2">
+      <div className="flex items-center justify-center h-14 w-16 lg:h-20 lg:w-24 border-solid border-2 lg:border-4 border-[#FFBA80]">
+        <h1 className=" text-lg lg:text-2xl font-light lg:font-medium text-[#FFBA80]">M C R</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Logotipo;
