69d198fd4dd4b6f44b7c2ee28c781d757791f590
Brandon Pinzon <brandonpinzont@gmail.com>
2025-01-03 18:56:38 -0500

    Add SeccionSalas

---
+++ src/components/SeccionSalas.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const SeccionSalas = () => {
+  return (
+<section
+  id="Salas"
+  className="flex min-h-screen w-full items-center justify-center">
+    <Image
+     src="/images/furniture/sofa-cuero.jpg"
+     fill={true}
+     className="object-cover object-cente z-10"
+     alt=""
+    />
+    <div className=" absolute inset-0 bg-gradient-to-t from-[#19120c] z-20"></div>
+    <h1 className="text-9xl text-[#ffba80] z-30">Salas</h1>
+  </section>  )
+}
+
+export default SeccionSalas
